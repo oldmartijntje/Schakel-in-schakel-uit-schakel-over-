1cbc8b7d2bd72eeca7a819f43510c94f69315d55
oldmartijntje <oldmartijntje@gmail.com>
2021-12-07 13:48:32 +0100

    final commit

--- lightswitch.py
+++ lightswitch.py
@@ -5,17 +5,21 @@ def aanUit():
         lichtAanOfUit = False
         print("light is off")
         button["text"] = "Switch Light On"
+        window["bg"] = "black"
     elif lichtAanOfUit == False:
         lichtAanOfUit = True
         print("light is on")
         button["text"] = "Switch Light Off"
+        window["bg"] = "yellow"
     
 
 window = tk.Tk()
+window.geometry('300x200')
+window.configure(bg='yellow')
 button = tk.Button(text='Switch Light Off', bg="white", fg="black", command=aanUit)
 button.pack(pady = 20, padx = 20)
-
 # schijf hier tussen je code
+window.title("what if i don't want the title to be TK?")
 lichtAanOfUit = True
 # schijf hier tussen je code
 
