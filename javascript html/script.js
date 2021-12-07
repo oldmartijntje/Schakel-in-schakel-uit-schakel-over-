222a45184725bbda744b4a7845a5e4251fa68961
oldmartijntje <oldmartijntje@gmail.com>
2021-12-07 14:34:27 +0100

    i did something

--- javascript html/script.js
+++ javascript html/script.js
@@ -1,10 +1,20 @@
+function myFunction() {
+    document.getElementById("btn").innerHTML = "Hello World";
+    document.getElementById("btn").style.color = "red";
+  }
+
 var btn = document.createElement('button');
 btn.style.margin = '10px';
-btn.innerHTML = '...';
+btn.innerHTML = 'button';
 document.body.appendChild(btn);
 
+  
 // schijf hier tussen je code
 
-
+var button = document.querySelector('button');
+button.onclick = function() {
+  //do stuff
+  alert("button was clicked");
+}
 
 // schijf hier tussen je code
