8606c88232790454c1573b2f70a2c680ca0a8d79
oldmartijntje <oldmartijntje@gmail.com>
2021-12-07 13:07:03 +0100

    2

--- lightswitch.py
+++ lightswitch.py
@@ -1,10 +1,18 @@
 import tkinter as tk
+def aanUit():
+    global lichtAanOfUit
+    if lichtAanOfUit == True:
+        lichtAanOfUit = False
+    elif lichtAanOfUit == False:
+        lichtAanOfUit = True
+    print(lichtAanOfUit)
+
 window = tk.Tk()
-button = tk.Button(text='kaas', bg="white", fg="black")
+button = tk.Button(text='lichtknopje', bg="white", fg="black", command=aanUit)
 button.pack(pady = 20, padx = 20)
 
 # schijf hier tussen je code
-
+lichtAanOfUit = True
 greeting = tk.Label(text="text")
 greeting.pack()
 print("no")
